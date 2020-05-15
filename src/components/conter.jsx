4ba9ddb0df5587b03a259781a720ac95f8c1b393
Ashley Holbrook <ashley@crema.us>
2020-05-15 15:49:13 -0500

    test

--- src/components/conter.jsx
+++ src/components/conter.jsx
@@ -1,11 +1,16 @@
 //imrc
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 //cc
 class Counter extends Component {
-    render() { 
-        return <h1>Hello World</h1>;
-    }
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <button>Increment</button>
+      </div>
+    );
+  }
 }
 
 export default Counter;
